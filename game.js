ea2e8ac83bdf9eaff083325a04832f99eafb0e84
Pablo <royadeveloper01@gmail.com>
2020-06-13 22:51:22 +0000

    added a progress bar

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = [];
 let acceptingAnswers = false;
@@ -53,7 +54,10 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     } 
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`
+    // Update on the progress bar
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+    
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -78,6 +82,10 @@ choices.forEach(choice => {
 
         const classToApply =
             selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+            if(classToApply === 'correct'){
+                incrementScore(CORRECT_BONUS)
+            }
         
 
         selectedChoice.parentElement.classList.add(classToApply);
@@ -89,5 +97,10 @@ choices.forEach(choice => {
         
     });
 });
+
+incrementScore = (num) => {
+    score +=num;
+    scoreText.innerText = score;
+}
 
 startGame();
