cd5b987d4c3837ecd623d67b371b9f7d6b980451
Pablo <royadeveloper01@gmail.com>
2020-06-22 16:44:46 +0000

    fetched questions

--- game.js
+++ game.js
@@ -9,32 +9,19 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let questions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    }, {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    }, {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
-
+fetch("https://opentdb.com/api.php?amount=10&category=28&difficulty=easy&type=multiple")
+.then(Response => {
+    console.log(Response.json());
+})
+.then(loadedQuestions => {
+    questions = loadedQuestions;
+    startGame();
+})
+.catch(err => {
+    console.error(err, "You have an error")
+});
 
 // CONSTANTS
 
@@ -103,5 +90,3 @@ incrementScore = (num) => {
     score +=num;
     scoreText.innerText = score;
 }
-
-startGame();
